a4cd062d1bad1576491e537b8d92ea98693180fc
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2023-03-13 18:14:12 +0100

    update fixed

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 let quoteId = document.getElementById("advice-id");
-  quoteId.innerHTML = 1;
-  let count = 1;
+quoteId.innerHTML = 1;
+let count = 0;
+let quote = document.querySelector('q');
 
- 
-  
-  let quote = document.getElementById("quote");
+let btn = document.querySelector(".btn").addEventListener("click",getAdvice);
+
+
   function getAdvice() {
     fetch("https://api.adviceslip.com/advice")
     .then(res => res.json())
     .then(function(data) {
       console.log(data);
       quote.innerHTML = data.slip.advice;
-      
-    
     })
     
     count += 1
       quoteId.innerHTML = count;
   }
+  
+  
+(function(){
+  getAdvice();
+}())
